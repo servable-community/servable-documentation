68b48da40f68389218aa854960b50c55285cb366
Aboubacar Doucoure <yelounak@gmail.com>
2023-03-04 19:20:44 +0100

    Theme

--- src/components/project/homePageHeader/index.js
+++ src/components/project/homePageHeader/index.js
@@ -6,58 +6,54 @@ import LogoStatic from '../logo/static'
 
 export default () => {
   const { siteConfig } = useDocusaurusContext()
-  return (
-    <header>
-      <div className={`
+  return <div className={`
         bg-gradient-to-r         
         from-[#E76F51]
         via-[#E76F51]
         to-[#E76F51]
         bg-opacity-20        
        border-b-[#FAF3DD]        
-        min-h-[600px]`}>
-        <div class={`
+        `}>
+    <div class={`
         grid
         grid-cols-1
         md:grid-cols-2
         px-6
         md:px-6      
         py-4`}>
-          <div class={`
+      <div class={`
           text-right  
           flex
           justify-center      
           align-middle                
           h-full
         `}>
-            {/* <LogoAnimated /> */}
-            <LogoStatic />
-          </div>
-          <div class={`                
+        {/* <LogoAnimated /> */}
+        <LogoStatic />
+      </div>
+      <div class={`                
           flex
           justify-center       
           p-8
         `}>
-            <div class={`
+        <div class={`
           justify-center
           text-left
           pt-12              
         `}>
-              <h1 className='text-9xl text-[#FAF3DD]'>{siteConfig.title}</h1>
-              <p className={`font-bold text-lg text-[#FAF3DD]`}>
-                {`A Node JS framework built on top of Parse Server and tailored for protocols. It provides a concise, easy to use and scalable template to get the best of protocols.\n Get ready to change the way you code your server!`}
-              </p>
-              <div className={``}>
-                <Link
-                  className="button button--secondary button--lg"
-                  to="/docs/getstarted/overview">
-                  Get started
-                </Link>
-              </div>
-            </div>
+          <h1 className='text-9xl text-[#FAF3DD]'>{siteConfig.title}</h1>
+          <p className={`font-bold text-lg text-[#FAF3DD]`}>
+            {`A Node JS framework built on top of Parse Server and tailored for protocols. It provides a concise, easy to use and scalable template to get the best of protocols.\n Get ready to change the way you code your server!`}
+          </p>
+          <div className={``}>
+            <Link
+              className="button button--secondary button--lg"
+              to="/docs/getstarted/overview">
+              Get started
+            </Link>
           </div>
         </div>
       </div>
-    </header>
-  )
+    </div>
+  </div>
 }
