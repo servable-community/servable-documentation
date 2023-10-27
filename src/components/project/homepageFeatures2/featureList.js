30d1f6aec1ad7039ad36c639004e7d5b1b4158bd
Aboubacar Doucoure <yelounak@gmail.com>
2023-10-27 14:11:09 +0200

    Changed seed icon

--- src/components/project/homepageFeatures2/featureList.js
+++ src/components/project/homepageFeatures2/featureList.js
@@ -12,7 +12,7 @@ export default [
   },
   {
     title: 'Automatic class and protocol seed',
-    Svg: require('@site/static/img/seed.svg').default,
+    Svg: require('@site/static/img/noun-seed-4962548.svg').default,
     description: (
       <>
         Servable Automatic Seed is an easy way to seed Servable classes. It was built from the ground up to respect the protocol lifecycle and separation of concerns. Every protocol can define config entries that drive its behaviour at run time, including the app protocol.
