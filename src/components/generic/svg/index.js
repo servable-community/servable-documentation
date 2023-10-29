9b64ac944ed025225492c08e79d7dd93ca9af993
Aboubacar Doucoure <yelounak@gmail.com>
2023-10-29 10:35:44 +0100

    chore(module): moved protocols to root

--- src/components/generic/svg/index.js
+++ src/components/generic/svg/index.js
@@ -3,8 +3,8 @@ import { useColorMode } from '@docusaurus/theme-common'
 
 export default ({ Svg, file, title, description, className = '', role = "img" }) => {
   const { isDarkTheme } = useColorMode()
-  const _Svg = Svg ? Svg : require(file).default
-  return <_Svg
+
+  return <Svg
     fill={`${isDarkTheme ? 'white' : 'black'}`}
     className={` 
             ${className}         
