7ed533858bf5c85210a089e2f62122c22c7f9ec4
Aboubacar Doucoure <yelounak@gmail.com>
2023-11-20 10:51:16 +0100

    Added widget components

--- src/components/domain/widget/index.js
+++ src/components/domain/widget/index.js
@@ -1,19 +1,20 @@
 import { Helmet } from "react-helmet"
 import React from "react"
 
-export default props =>{
-const {protocolId} = props
-return <div className="application">
-        <div id={protocolId}
-        data-protocolid={protocolId}
-        class="servable-protocol-widget"
-                // style="width: 250px; margin: auto; margin-top: 20px"
-                >
-                    <h4>Protocol</h4>
-                </div>
+export default props => {
+    const { protocolId, size = 's' } = props
+    return <div className="">        
+            <div id={protocolId}
+                data-protocolid={protocolId}
+                data-size={size}
+                class="servable-protocol-widget"
+            // style="width: 250px; margin: auto; margin-top: 20px"
+            >
+                <h4>Protocol</h4>
+            </div>        
         <Helmet>
             <link href="https://api.cdn.registry.servablecommunity.com/widgets/protocol/index.css" rel="stylesheet" />
-            <script async src="https://api.cdn.registry.servablecommunity.com/widgets/protocol/index.js"></script>                   
+            <script async src="https://api.cdn.registry.servablecommunity.com/widgets/protocol/index.js"></script>
             {/* <link href="http://localhost:3080/widget/index.css" rel="stylesheet" />
             <script async src="http://localhost:3080/widget/index.js"></script> */}
         </Helmet>
